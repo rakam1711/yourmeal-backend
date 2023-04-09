090e69e3505458ed1c3298a181ba36299065ffcc
theflippantfox <theflippantfox@gmail.com>
2023-04-09 10:55:54 +0530

    handling merger conflicts

--- src/controllers/resturant.controller.ts
+++ src/controllers/resturant.controller.ts
@@ -6,32 +6,13 @@ const { Resturants, MenuItems } = db
 // Resturant Auth Funtions
 export const loginResturantController = async (req: Request): Promise<DataReturnType<any>> => {
   const { email, password } = req.body
-<<<<<<< HEAD
-  const errors: string[] = []
-  const data: {token?: string} = {}
-
-  try {
-    const resturant = await db.Resturants.findOne({email})
-
-    data.token = resturant?.getSignedToken()
-=======
-
   const errors: string[] = []
   const data: { token?: string } = {}
 
   try {
-    const resturant = await Resturants.findOne({ email })
-    if (!resturant) {
-      throw new Error('Resturant does not exists')
-    }
+    const resturant = await db.Resturants.findOne({ email })
 
-    const isPasswordMatch = resturant.verifyPassword(password)
-    if (!isPasswordMatch) {
-      throw new Error('Email and Password do not match')
-    }
-
-    data.token = resturant.getSignedToken()
->>>>>>> 363e36efd60bc92b1a51ffec0d3a246a44257d0e
+    data.token = resturant?.getSignedToken()
   } catch (error) {
     errors.push(error as string)
     console.log(error)
@@ -57,11 +38,7 @@ export const registerResturantController = async (req: Request): Promise<DataRet
   } = req.body
 
   const errors: string[] = []
-<<<<<<< HEAD
-  const data: {token?: string} = {}
-=======
   const data: { token?: string } = {}
->>>>>>> 363e36efd60bc92b1a51ffec0d3a246a44257d0e
 
   try {
     const resturant = await db.Resturants.create({
@@ -76,10 +53,6 @@ export const registerResturantController = async (req: Request): Promise<DataRet
     })
 
     data.token = resturant.getSignedToken()
-<<<<<<< HEAD
-    
-=======
->>>>>>> 363e36efd60bc92b1a51ffec0d3a246a44257d0e
   } catch (error) {
     errors.push(error as string)
     console.log(error)
@@ -93,7 +66,6 @@ export const registerResturantController = async (req: Request): Promise<DataRet
 }
 
 // Other Resturant Functions
-<<<<<<< HEAD
 export const addMenuItemController = async (req: Request): Promise<DataReturnType<any>> => {
   const {
     name,
@@ -105,13 +77,13 @@ export const addMenuItemController = async (req: Request): Promise<DataReturnTyp
     tags
   } = req.body
 
-  const errors:string[] = []
+  const errors: string[] = []
   const data: { item?: any } = {}
 
-  try{
+  try {
     const item = await db.MenuItems.create({
       resturant: req.resturant,
-      name,    
+      name,
       description,
       thumbnail,
       price,
@@ -123,35 +95,19 @@ export const addMenuItemController = async (req: Request): Promise<DataReturnTyp
     console.log(item)
 
     data.item = item
-  } catch(error) {
-    errors.push(error as string)
-  }
-  
-  return {
-    success: errors.length < 1,
-=======
-export const resturantAddMenuItemController = async (req: Request): Promise<DataReturnType<any>> => {
-  const errors: string[] = []
-  const data = {}
-  const { hi } = req.body
-
-  try {
-    console.log(hi)
   } catch (error) {
     errors.push(error as string)
   }
 
   return {
-    success: Object.keys(errors).length < 1,
->>>>>>> 363e36efd60bc92b1a51ffec0d3a246a44257d0e
+    success: errors.length < 1,
     errors,
     data
   }
 }
-<<<<<<< HEAD
 
 export const getResturantMenuController = async (req: Request): Promise<DataReturnType<any>> => {
-  const errors:string[] = []
+  const errors: string[] = []
   const data: { items?: any[] } = {}
 
   try {
@@ -160,12 +116,10 @@ export const getResturantMenuController = async (req: Request): Promise<DataRetu
   } catch (error) {
     errors.push(error as string)
   }
-  
+
   return {
     success: errors.length < 1,
     errors,
     data
   }
 }
-=======
->>>>>>> 363e36efd60bc92b1a51ffec0d3a246a44257d0e
