7fd42ac4303429d23b127ca110183bd5c847bc0e
TheFlippantFox <theflippantfox@gmail.com>
2023-05-03 09:03:24 +0000

    clint side fix

--- src/middlewares/protect.ts
+++ src/middlewares/protect.ts
@@ -4,36 +4,40 @@ import jwt from 'jsonwebtoken'
 import { JWT_SECRET_KEY } from '../configs/constants'
 
 const protect = async (req: any, res: any, next: any): Promise<void> => {
-  const { authorization } = req.headers
+  try {
+    const { authorization } = req.headers
 
-  if (!authorization) {
-    res.json({
-      success: false,
-      errors: { message: 'User not logged in' },
-      data: {}
-    }).status(404)
-    return
-  }
-
-  const token = authorization.split(' ')[1]
-  const data: any = jwt.verify(token, JWT_SECRET_KEY)
-
-  if (data.role === 'user') {
-    const user = await db.Users.findById(data?.userid)
-    if (!user) {
-      res.json({ success: false, message: 'User does not exist ' }).status(404)
+    if (!authorization) {
+      res.json({
+        success: false,
+        errors: { message: 'User not logged in' },
+        data: {}
+      }).status(404)
+      return
     }
-    req.user = data.userid
-  }
 
-  if (data.role === 'resturant') {
-    const resturant = await db.Resturants.findById(data?.resturantid)
-    if (!resturant) {
-      res.json({ success: false, message: 'Resturant does not exist ' }).status(404)
+    const token = authorization.split(' ')[1]
+    const data: any = jwt.verify(token, JWT_SECRET_KEY)
+
+    if (data.role === 'user') {
+      const user = await db.Users.findById(data?.userid)
+      if (!user) {
+        res.json({ success: false, message: 'User does not exist ' }).status(404)
+      }
+      req.user = data.userid
     }
-    req.resturant = data.resturantid
+
+    if (data.role === 'resturant') {
+      const resturant = await db.Resturants.findById(data?.resturantid)
+      if (!resturant) {
+        res.json({ success: false, message: 'Resturant does not exist ' }).status(404)
+      }
+      req.resturant = data.resturantid
+    }
+    next()
+  } catch (error) {
+    console.error(error)
   }
-  next()
 }
 
 export default protect
